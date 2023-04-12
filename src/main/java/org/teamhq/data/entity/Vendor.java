6d3c3325a63ebff7fc0ab7f96a49a0e257c58a50
taefi <taefi.soroosh@gmail.com>
2023-04-12 10:14:38 +0300

    extend entities from AbstractEntity

--- src/main/java/org/teamhq/data/entity/Vendor.java
+++ src/main/java/org/teamhq/data/entity/Vendor.java
@@ -1,26 +1,15 @@
 package org.teamhq.data.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
 @Entity
-public class Vendor {
-    @Id
-    private Number id;
+public class Vendor extends AbstractEntity {
 
     @NotEmpty
     private String name;
 
     private String description;
-
-    public Number getId() {
-        return id;
-    }
-
-    public void setId(Number id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
