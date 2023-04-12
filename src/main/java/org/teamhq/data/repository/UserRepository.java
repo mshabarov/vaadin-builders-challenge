9da8c6516028f097313ae1cbbc281139ac3343fd
taefi <taefi.soroosh@gmail.com>
2023-04-12 10:35:43 +0300

    add initial repositories

--- src/main/java/org/teamhq/data/repository/UserRepository.java
+++ src/main/java/org/teamhq/data/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package org.teamhq.data.service;
+package org.teamhq.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
